1bff8140afe01fb95bde99990e4177aa30345cee
Joe Maller <joe@joemaller.com>
2014-01-06 11:54:32 -0500

    first commit

---
+++ src/Feed/GetFeed.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Feed;
+
+class GetFeed
+{
+    public static function fake()
+    {
+        return "Hi, I'm fake";
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Feed\GetFeed;
+
+
+echo GetFeed::fake();
